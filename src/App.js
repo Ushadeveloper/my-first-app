e1fb3f25fa4e85d7835bf750df1e2106973d40db
U_s H_a <ushausama2@outlook.com>
2024-01-03 18:04:59 +0500

    Add css only in Simple Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MyButton from './Components/Buttons.Component';
+import { StringName } from './Components/Buttons.Component';
+import Forms from './Components/Form.Component';
+import './css/style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my app</h1>
+      {/* <MyButton />
+      <StringName></StringName> */}
+      {/* <Forms></Forms> */}
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/Components/Buttons.Component.jsx
@@ -0,0 +1,21 @@
+function MyButton() {
+    return (
+        <>
+            <h2>Nechy Dekho Nechy</h2>
+            <button>I'm a button</button>
+        </>
+    );
+}
+
+
+export default MyButton;
+
+
+export function StringName(){
+    return(
+        <>
+        <h2>Uper Button h ..........</h2>
+        
+        </>
+    );
+}
